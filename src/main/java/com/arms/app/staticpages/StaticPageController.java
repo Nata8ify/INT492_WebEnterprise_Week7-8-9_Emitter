3cbcb810c3f1e94618e1c4c575c6fa7710a7c505
NATTAWUT PICHAIWATTNPHON <Nata8ify@users.noreply.github.com>
2017-09-14 14:43:55 +0700

    OVERRIDE Default Authentication Password

---
+++ src/main/java/com/arms/app/staticpages/StaticPageController.java
@@ -0,0 +1,15 @@
+package com.arms.app.staticpages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StaticPageController {
+	
+
+	@RequestMapping("/help")
+	public String help(){
+		return "static/help";
+	}
+
+}
