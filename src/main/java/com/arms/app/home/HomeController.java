0f114e92f70f07e6c93b80e98287dd57f5d86d6d
NATTAWUT PICHAIWATTNPHON <nata8ify@gmail.com>
2017-10-20 15:11:29 +0700

    MAKE Post Pagination

--- src/main/java/com/arms/app/home/HomeController.java
+++ src/main/java/com/arms/app/home/HomeController.java
@@ -1,19 +1,28 @@
 package com.arms.app.home;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.arms.domain.component.ControllerAspect;
+import com.arms.domain.component.PageWrapper;
+import com.arms.domain.entity.Micropost;
+import com.arms.domain.service.HomeService;
 
 @Controller
 public class HomeController {
 	@Autowired
 	ControllerAspect controllerAspect;
+
+	@Autowired
+	HomeService homeService;
 
 	@ModelAttribute
 	MicropostCreateForm setMicropostCreateForm() {
@@ -21,8 +30,15 @@ public class HomeController {
 	}
 
 	@RequestMapping("/")
-	public ModelAndView home(ModelAndView modelAndView, Principal principal) {
-		// controllerAspect.setIsLogin(modelAndView, principal);
+	public ModelAndView home(ModelAndView modelAndView, Principal principal, Pageable pageable) {
+		Integer userId = homeService.getUserId(principal);
+		if (userId != null) {
+			List<Integer> micropostIdList = homeService.getMyMicropost(userId);
+			Page<Micropost> micropostPage = homeService.findAllByIdList(micropostIdList, pageable);
+			PageWrapper<Micropost> page = new PageWrapper<>(micropostPage, "/");
+			modelAndView.addObject("microposts", page.getContent());
+			modelAndView.addObject("page", page);
+		}
 		modelAndView.setViewName("home/home");
 		return modelAndView;
 	}
--- src/main/java/com/arms/domain/repository/MicropostRepository.java
+++ src/main/java/com/arms/domain/repository/MicropostRepository.java
@@ -1,5 +1,10 @@
 package com.arms.domain.repository;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +12,7 @@ import com.arms.domain.entity.Micropost;
 
 @Repository
 public interface MicropostRepository extends JpaRepository<Micropost, Integer> {
-	
+	List<Micropost> findAllByUserId(int userId);
+	Page<Micropost> findAllByUserIdOrderByUpdatedDesc(int userId, Pageable pageable);
+	Page<Micropost> findByIdInOrderByUpdatedDesc(Collection id, Pageable pageable);
 }
